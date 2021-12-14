2da5df8bd207efdda74c58f096e5421633d580a7
Smalz92 <1694589757@qq.com>
2021-12-14 08:19:50 +0800

    使用定长数组代替vector

--- leetcode/Day02_506.相对名次/relativeRank.cpp
+++ leetcode/Day02_506.相对名次/relativeRank.cpp
@@ -10,19 +10,22 @@ using namespace std;
 class Solution {
 public:
 	vector<string> findRelativeRanks(vector<int>& score) {
-		vector<int> copy = score;
+		//vector<int> copy = score;
 		const int len = score.size();
 		if (len == 1) return { "Gold Medal" };
-		sort(score.begin(), score.end(), greater<int>());  /* 从大到小排序, 排序后长度不变, 故len还能用 */
+		int *arr = new int[len];
+		std::copy(score.begin(),score.end(),arr);
+		sort(arr,arr + len, greater<int>());
+		//sort(score.begin(), score.end(), greater<int>());  /* 从大到小排序, 排序后长度不变, 故len还能用 */
 		// 将原位置上的每个数的rank放进dict
 		unordered_map<int, string> rankDict;  // map: score -> rank (string)
-		rankDict[score[0]] = "Gold Medal";
-		rankDict[score[1]] = "Silver Medal";
-		if (len > 2) rankDict[score[2]] = "Bronze Medal";
-		for (int i = 3; i < len; i++) rankDict[score[i]] = to_string(i + 1);
+		rankDict[arr[0]] = "Gold Medal";
+		rankDict[arr[1]] = "Silver Medal";
+		if (len > 2) rankDict[arr[2]] = "Bronze Medal";
+		for (int i = 3; i < len; i++) rankDict[arr[i]] = to_string(i + 1);
 		vector<string> res;
 		for (int i = 0; i < len; i++)
-			res.push_back(rankDict[copy[i]]);
+			res.push_back(rankDict[score[i]]);
 		return res;
 	}
 };
